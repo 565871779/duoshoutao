1704e7a81b6edf190fb9e90a4fcd7022f296481a
565871779 <565871779@qq.com>
2019-02-24 17:14:34 +0800

    update

---
+++ duoshoutao_vue/src/pages/home/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="root">
+      <bottom></bottom>
+  </div>
+</template>
+
+<script>
+import bottom from './index_bottom'
+export default {
+  components: {
+    bottom
+  },
+  data () {
+    return {
+    }
+  },
+  computed: {},
+  methods: {},
+  mounted () {}
+}
+</script>
+<style lang="scss" scoped>
+.root{}
+</style>
---
+++ duoshoutao_vue/src/pages/home/index_bottom.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="bottom">
+      <div class="buttons">
+          <div class="item">
+              <div class="button">
+                  促
+              </div>
+              <p>促销卖场</p>
+          </div>
+          <div class="item">
+              <div class="button">
+                  促
+              </div>
+              <p>促销卖场</p>
+          </div>
+           <div class="item">
+              <div class="button">
+                  促
+              </div>
+              <p>促销卖场</p>
+          </div>
+           <div class="item">
+              <div class="button">
+                  促
+              </div>
+              <p>促销卖场</p>
+          </div>
+           <div class="item">
+              <div class="button">
+                  促
+              </div>
+              <p>促销卖场</p>
+          </div>
+           <div class="item">
+              <div class="button">
+                  促
+              </div>
+              <p>促销卖场</p>
+          </div>
+           <div class="item">
+              <div class="button">
+                  促
+              </div>
+              <p>促销卖场</p>
+          </div>
+           <div class="item">
+              <div class="button">
+                  促
+              </div>
+              <p>促销卖场</p>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data () {
+    return {
+    }
+  },
+  computed: {},
+  methods: {},
+  mounted () {}
+}
+</script>
+<style lang="scss" scoped>
+.bottom{
+    width: 100%;
+    .buttons {
+        height: 2.5rem;
+        display: flex;
+        flex-wrap: wrap;
+        padding: .4rem;
+        justify-content: space-between;
+    }
+    .item {
+        width: 21%;
+        height: 1.2rem;
+        margin-top: .5rem;
+        margin-bottom: .3rem;
+        .button {
+            width: 1.2rem;
+            height: 1.2rem;
+            border-radius: 50%;
+            border: 1px solid #ccc;
+            margin-right: .4rem;
+        }
+        p {
+            margin-top: .2rem;
+            margin-left: .1rem;
+        }
+    }
+}
+</style>
