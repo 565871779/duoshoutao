427426f610a3e5a0712ef1d2521487d53b2ff49d
565871779 <565871779@qq.com>
2019-03-27 21:36:28 +0800

    update

---
+++ duoshoutao_admin/src/pages/login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="root"></div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data () {
+    return {
+    }
+  },
+  computed: {},
+  methods: {},
+  mounted () {}
+}
+</script>
+<style lang="scss" scoped>
+.root{}
+</style>
